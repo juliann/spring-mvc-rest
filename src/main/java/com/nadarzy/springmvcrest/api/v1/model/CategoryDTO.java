6b020e20181f5f1c4cfe0a25c951484c502fd48e
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-09-01 12:59:59 +0200

    added mapstruct, bootstraped initial data(categories)

--- src/main/java/com/nadarzy/springmvcrest/api/v1/model/CategoryDTO.java
+++ src/main/java/com/nadarzy/springmvcrest/api/v1/model/CategoryDTO.java
@@ -1,6 +1,12 @@
 package com.nadarzy.springmvcrest.api.v1.model;
 
-/** */
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class CategoryDTO {
 
   private Long id;
--- src/main/java/com/nadarzy/springmvcrest/model/Category.java
+++ src/main/java/com/nadarzy/springmvcrest/model/Category.java
@@ -1,13 +1,17 @@
 package com.nadarzy.springmvcrest.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Data
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Category {
 
